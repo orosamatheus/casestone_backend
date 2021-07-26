c8100c6872e8be58ae43dc2bb938f670f5ca4671
Matheus Rosa <matheusrosa.la@gmail.com>
2021-07-26 18:22:20 -0300

    feat: error handling on application

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,32 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 
+import "reflect-metadata";
+import "express-async-errors";
+import { AppError } from "./errors/AppError";
 import { router } from "./routes";
 
 import "./database";
-import "reflect-metadata";
+
+import "./shared/container";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+        if (err instanceof AppError) {
+            return response.status(err.statusCode).json({
+                message: err.message,
+            });
+        }
+
+        return response.status(500).json({
+            status: "error",
+            message: `Internal server error - ${err.message}`,
+        });
+    }
+);
 
 app.listen(3333, () => console.log("Case stone server is running"));
 
