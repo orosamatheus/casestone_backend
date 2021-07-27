27be7b29fe730d839ed284bcb7a983a8dbdf73d7
Matheus Rosa <matheusrosa.la@gmail.com>
2021-07-27 14:11:58 -0300

    feat: tsyringe singleton UsersCharactersRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,18 @@
 import { container } from "tsyringe";
 
+// Users
 import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
 import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
+// users_characters
+import { UsersCharactersRepository } from "../../modules/characters/repositories/implementations/UsersCharactersRepository";
+import { IUsersCharactersRepository } from "../../modules/characters/repositories/IUsersCharactersRepository";
 
 container.registerSingleton<IUsersRepository>(
     "UsersRepository",
     UsersRepository
+);
+
+container.registerSingleton<IUsersCharactersRepository>(
+    "UsersCharactersRepository",
+    UsersCharactersRepository
 );
