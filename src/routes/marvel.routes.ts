b5a0c6e8483de3522accfc008b0fe5624cf495f5
Matheus Rosa <matheusrosa.la@gmail.com>
2021-07-28 16:30:48 -0300

    feat: comic and character by id

--- src/routes/marvel.routes.ts
+++ src/routes/marvel.routes.ts
@@ -11,6 +11,16 @@ marvelRoutes.get("/characters", async (req, res) => {
     return res.json(response.data.data.results);
 });
 
+marvelRoutes.get("/characterId/:character_id", async (req, res) => {
+    const { character_id } = req.params;
+
+    const response = await axios.get(
+        `https://gateway.marvel.com/v1/public/characters/${character_id}?ts=1627477497&apikey=e727c9747111b13e942697e461e47a93&hash=8c04fa1fdae445d5e184376ceebd65a2`
+    );
+
+    return res.json(response.data.data.results);
+});
+
 marvelRoutes.get("/characters/:name", async (req, res) => {
     const { name } = req.params;
 
@@ -29,6 +39,23 @@ marvelRoutes.get("/comics/:title", async (req, res) => {
     );
 
     return res.json(response.data.data.results);
+});
+
+marvelRoutes.get("/comicId/:comic_id", async (req, res) => {
+    try {
+        const { comic_id } = req.params;
+
+        const response = await axios.get(
+            `https://gateway.marvel.com/v1/public/comics/${comic_id}?ts=1627477497&apikey=e727c9747111b13e942697e461e47a93&hash=8c04fa1fdae445d5e184376ceebd65a2`
+        );
+        console.log(comic_id);
+        console.log(response.data);
+
+        return res.json(response.data.data.results);
+    } catch (err) {
+        console.log(err);
+        throw new Error(err.message);
+    }
 });
 
 export { marvelRoutes };
