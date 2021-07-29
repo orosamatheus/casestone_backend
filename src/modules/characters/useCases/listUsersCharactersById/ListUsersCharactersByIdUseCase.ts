13534f18fb03299aa32b17bc16e125ac066b24da
Matheus Rosa <matheusrosa.la@gmail.com>
2021-07-29 13:48:37 -0300

    return comic && character on useCase

--- src/modules/characters/useCases/listUsersCharactersById/ListUsersCharactersByIdUseCase.ts
+++ src/modules/characters/useCases/listUsersCharactersById/ListUsersCharactersByIdUseCase.ts
@@ -1,13 +1,17 @@
 import { inject, injectable } from "tsyringe";
 
 import { UserCharacters } from "../../entities/UserCharacters";
+import { CharactersProvider } from "../../providers/implementations/CharactersProvider";
 import { UsersCharactersRepository } from "../../repositories/implementations/UsersCharactersRepository";
 
 @injectable()
 class ListUsersCharactersByIdUseCase {
     constructor(
         @inject("UsersCharactersRepository")
-        private usersCharactersRepository: UsersCharactersRepository
+        private usersCharactersRepository: UsersCharactersRepository,
+
+        @inject("CharactersProvider")
+        private charactersProvider: CharactersProvider
     ) {}
 
     async execute(user_id: string): Promise<UserCharacters[]> {
@@ -16,7 +20,17 @@ class ListUsersCharactersByIdUseCase {
                 user_id
             );
 
-        return usersCharacters;
+        const list = await Promise.all(
+            usersCharacters.map(async (userCharacter: UserCharacters) => {
+                // eslint-disable-next-line no-param-reassign
+                userCharacter.character = await this.charactersProvider.getById(
+                    userCharacter.character_id
+                );
+                return userCharacter;
+            })
+        );
+
+        return list;
     }
 }
 
--- src/modules/comics/useCases/listUsersComicsById/ListUsersComicsByIdUseCase.ts
+++ src/modules/comics/useCases/listUsersComicsById/ListUsersComicsByIdUseCase.ts
@@ -1,13 +1,17 @@
 import { inject, injectable } from "tsyringe";
 
 import { UserComics } from "../../entities/UserComics";
+import { ComicsProvider } from "../../providers/implementations/ComicsProvider";
 import { UsersComicsRepository } from "../../repositories/implementations/UsersComicsRepository";
 
 @injectable()
 class ListUsersComicsByIdUseCase {
     constructor(
         @inject("UsersComicsRepository")
-        private usersComicsRepository: UsersComicsRepository
+        private usersComicsRepository: UsersComicsRepository,
+
+        @inject("ComicsProvider")
+        private comicsProvider: ComicsProvider
     ) {}
 
     async execute(user_id: string): Promise<UserComics[]> {
@@ -15,7 +19,17 @@ class ListUsersComicsByIdUseCase {
             user_id
         );
 
-        return userComics;
+        const list = await Promise.all(
+            userComics.map(async (userComic: UserComics) => {
+                // eslint-disable-next-line no-param-reassign
+                userComic.comic = await this.comicsProvider.getById(
+                    userComic.comic_id
+                );
+                return userComic;
+            })
+        );
+
+        return list;
     }
 }
 
