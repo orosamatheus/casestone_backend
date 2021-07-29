df5fa0d421440bffeab704c08db9d25922389139
Matheus Rosa <matheusrosa.la@gmail.com>
2021-07-29 13:46:37 -0300

    feat: register Comics and Character providers

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -4,9 +4,13 @@ import { container } from "tsyringe";
 import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
 import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
 // users_characters
+import { ICharactersProvider } from "../../modules/characters/providers/ICharactersProvider";
+import { CharactersProvider } from "../../modules/characters/providers/implementations/CharactersProvider";
 import { UsersCharactersRepository } from "../../modules/characters/repositories/implementations/UsersCharactersRepository";
 import { IUsersCharactersRepository } from "../../modules/characters/repositories/IUsersCharactersRepository";
 // users_comics
+import { IComicsProvider } from "../../modules/comics/providers/iComicsProvider";
+import { ComicsProvider } from "../../modules/comics/providers/implementations/ComicsProvider";
 import { UsersComicsRepository } from "../../modules/comics/repositories/implementations/UsersComicsRepository";
 import { IUsersComicsRepository } from "../../modules/comics/repositories/IUsersComicsRepository";
 
@@ -23,4 +27,11 @@ container.registerSingleton<IUsersCharactersRepository>(
 container.registerSingleton<IUsersComicsRepository>(
     "UsersComicsRepository",
     UsersComicsRepository
+);
+
+container.registerSingleton<IComicsProvider>("ComicsProvider", ComicsProvider);
+
+container.registerSingleton<ICharactersProvider>(
+    "CharactersProvider",
+    CharactersProvider
 );
